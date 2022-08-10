aa48233fe1a80f017922b9d2c664f5cf4d193740
IlkaCarmo <ilka_2009@ig.com.br>
2022-08-10 15:20:25 -0300

    Projeto iniciado

---
+++ Repository/context/BookStoreContext.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.context
+{
+    internal class BookStoreContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        public BookStoreContext(DbContextOptions<BookStoreContext> option) : base(option)
+        {
+        }
+
+    }
+}
